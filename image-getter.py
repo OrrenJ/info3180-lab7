d9a87c45973f85025a03e3c9d2bef53b4412af0a
Orren Joseph <ojey11@gmail.com>
2017-04-06 14:01:14 -0500

    Exercise 1

--- image-getter.py
+++ image-getter.py
@@ -2,24 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import urlparse
 
-url = "https://store.roosterteeth.com/"
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "html.parser")
+def get_images():
 
-# This will look for a meta tag with the og:image property
-og_image = (soup.find('meta', property='og:image') or
-                    soup.find('meta', attrs={'name': 'og:image'}))
-if og_image and og_image['content']:
-    print og_image['content']
-    print ''
+	img_list = []
 
-# This will look for a link tag with a rel attribute set to 'image_src'
-thumbnail_spec = soup.find('link', rel='image_src')
-if thumbnail_spec and thumbnail_spec['href']:
-    print thumbnail_spec['href']
-    print ''
+	url = "https://store.roosterteeth.com/"
+	result = requests.get(url)
+	soup = BeautifulSoup(result.text, "html.parser")
 
+	# This will look for a meta tag with the og:image property
+	og_image = (soup.find('meta', property='og:image') or
+	                    soup.find('meta', attrs={'name': 'og:image'}))
+	if og_image and og_image['content']:
+	    img_list.append(og_image['content'])
 
-for img in soup.findAll("img", src=True):
-   print urlparse.urljoin(url, img["src"])
-   print ''
+	# This will look for a link tag with a rel attribute set to 'image_src'
+	thumbnail_spec = soup.find('link', rel='image_src')
+	if thumbnail_spec and thumbnail_spec['href']:
+	    img_list.append(thumbnail_spec['href'])
+
+	for img in soup.findAll("img", src=True):
+	   img_list.append(urlparse.urljoin(url, img["src"]))
+
+	return img_list
+
